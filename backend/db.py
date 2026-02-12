9491f6945ed29d38e2064a4ce85be3706f39cbb3
yudinglin <158040037+yudinglin@users.noreply.github.com>
2026-02-12 20:59:56 +0800

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -7,7 +7,8 @@ load_dotenv()
 def get_connection():
     return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT", 3306)),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        database=os.getenv("DB_NAME"),
     )
