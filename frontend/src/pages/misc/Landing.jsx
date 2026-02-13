fdf0fab3648cf259e31bfac0df45b671281f8669
yudinglin <158040037+yudinglin@users.noreply.github.com>
2026-02-14 00:21:00 +0800

    Update Landing.jsx

--- frontend/src/pages/misc/Landing.jsx
+++ frontend/src/pages/misc/Landing.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { API_BASE } from "../../core/api/client";
 
 export default function Landing() {
   const [reviews, setReviews] = useState([]);
