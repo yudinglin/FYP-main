0a715bfe39c90f97736bbd8e682b9cdee4bdfae2
yudinglin <158040037+yudinglin@users.noreply.github.com>
2026-02-14 11:08:42 +0800

    Fix import statement for CORS in app.py

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 from db import get_connection
 
