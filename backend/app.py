cefef2a903cc9879dfc1703087e28126dd4349ab
yudinglin <158040037+yudinglin@users.noreply.github.com>
2026-02-14 11:10:20 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,6 @@ from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from dotenv import load_dotenv
-from flask_cors import CORS
 
 from db import get_connection
 
